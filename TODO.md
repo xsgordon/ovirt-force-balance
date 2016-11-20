1405e1721ea3c8a827a1e6648448f78041b510e5
Stephen Gordon <sgordon@redhat.com>
2016-11-19 22:31:32 -0500

    Add support for multi-cluster environments
    Add support for multi-cluster environments, by looping through the
    clusters in the environment and balancing each one independently.
    
    Alternatively a new --cluster/-c CLUSTER parameter has been added to run
    balancing only on a single cluster.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,4 @@
 TODO
 ====
 
-* Querying for hosts is currently not cluster specific as originally I built
-  this for an environment with only one cluster. Needs to be re-worked to do
-  balancing on a per-cluster basis.
 * Parameterize min/max watermarks for determining when to take action.
--- ovirt-force-balance.py
+++ ovirt-force-balance.py
@@ -12,20 +12,29 @@ parser.add_argument("-l", "--location", action="store",
                     default="https://127.0.0.1:8443")
 parser.add_argument("-u", "--user", action="store", default="admin@internal")
 parser.add_argument("-p", "--password", action="store", required=True)
+parser.add_argument("-c", "--cluster", action="store", required=False)
 args = parser.parse_args()
 
-try:
-    api = API(url=args.location,
-              username=args.user,
-              password=args.password,
-              insecure=True)
+
+def _balanceCluster(cluster):
+
+    print "Attempting to balance %s cluster (%s):" % (cluster.name, cluster.id)
 
     # Determine how many hosts and how many virtual machines are in the
     # environment. Use this to determine the ideal number of virtual machines
-    # per host. Note that this is currently a "dumb" calculation in that it
-    # assumes one cluster for the entire environment.
-    active_hosts = int(api.get_summary().get_hosts().active)
-    active_vms = int(api.get_summary().get_vms().active)
+    # per host.
+
+    host_list = api.hosts.list(query="cluster = %s and status = up" %
+                               cluster.name)
+
+    active_hosts = len(host_list)
+    active_vms = len(api.vms.list(query="cluster = %s and status = up" %
+                                  cluster.name))
+
+    if active_hosts == 0:
+        print "No active hosts in cluster %s to balance." % cluster.name
+        return
+
     ideal_load = active_vms / active_hosts
     ideal_min = ideal_load * 0.90
     ideal_max = ideal_load * 1.10
@@ -36,25 +45,13 @@ try:
     print "Ideal Virtual Machines per host: %s to %s" % (str(ideal_min),
                                                          str(ideal_max))
 
-    # Obtain a list of hosts being sure to retrieve all pages.
-    host_list = []
-    host_page_index = 1
-    host_page_current = api.hosts.list(query="page %s" % host_page_index)
-
-    while(len(host_page_current) != 0):
-        host_list = host_list + host_page_current
-        host_page_index = host_page_index + 1
-        try:
-            host_page_current = api.hosts.list(query="page %s" %
-                                               host_page_index)
-        except Exception as ex:
-            print "Error retrieving page %s of list: %s" % (host_page_index,
-                                                            ex)
+    if active_hosts == 1:
+        return
 
     # Loop through the hosts looking at the number of active VMs indicated. For
     # each host determine if it is over/under utilized. For over utilized
     # engage migration of enough VMs to bring it within range, for under
-    # utilized just skip with a message. This is also a "dumb" approach in that
+    # utilized just skip with a message. This is a "dumb" approach in that
     # it works best in small clusters, in larger clusters a migration target
     # should really be specified otherwise you might just move VMs from one
     # over utilized host to another.
@@ -76,6 +73,27 @@ try:
                     sleep(60)
         elif host_active < ideal_min:
             print "Host %s is under-utilized" % host.name
+
+
+try:
+    api = API(url=args.location,
+              username=args.user,
+              password=args.password,
+              insecure=True)
+
+    if args.cluster is None:
+
+        clusters = api.clusters.list()
+
+        for cluster in clusters:
+            _balanceCluster(cluster)
+
+    else:
+
+        clusters = api.clusters.list(query="name = %s" % args.cluster)
+
+        for cluster in clusters:
+            _balanceCluster(cluster)
 
 except Exception as ex:
     print "Unexpected error: %s" % ex
